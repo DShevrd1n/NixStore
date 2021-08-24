b95c6e5a09814d0563335ec4e1eeda41abb22b68
Sheverdin Denis <20shev02@gmail.com>
2021-08-24 03:17:21 +0300

    a

--- infrustructure/Memory/ProductRepository.cs
+++ infrustructure/Memory/ProductRepository.cs
@@ -11,7 +11,7 @@ namespace Memory
             new Product(1,"1754844","Wine"),
             new Product(2,"1700244", "Beer"),
             new Product(3,"1702844","Vodka"),
-            new Product(4,"1137844","Voda"),
+            new Product(4,"1137844","Viski"),
         };
 
         public Product[] GetAllByArticul(string articul)
